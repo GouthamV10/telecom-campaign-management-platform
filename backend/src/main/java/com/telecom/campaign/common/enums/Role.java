ee582efcb41bc7d4ec0670217544ceb455bc2f26
goutham.srinivasa <goutham.srinivasa@6dtech.co.in>
2026-07-26 19:10:05 +0530

    feat: added entity,dto classes for User

---
+++ backend/src/main/java/com/telecom/campaign/common/enums/Role.java
@@ -0,0 +1,7 @@
+package com.telecom.campaign.common.enums;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    AGENT
+}
---
+++ backend/src/main/java/com/telecom/campaign/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.telecom.campaign.user.entity;
+
+import com.telecom.campaign.common.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
