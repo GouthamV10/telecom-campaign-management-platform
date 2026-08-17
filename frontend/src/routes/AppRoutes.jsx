18d89df2e22681072400fb563c0c429b9fa919e1
Goutham V <goutham.v.16@gmail.com>
2026-08-18 00:28:23 +0530

    frontedn setup

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}/>
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
